c6e3e62668e994bbb2d80e3ec53bec87d28da0a7
Koojiny <msj09252@gmail.com>
2023-01-05 21:56:14 +0900

    Day 2 : Operator

--- src/chap_02/_01_Operator1.java
+++ src/chap_02/_01_Operator1.java
@@ -9,6 +9,63 @@ public class _01_Operator1 {
         System.out.println(4 - 2); // 2
         System.out.println(4 * 2); // 8
         System.out.println(4 / 2); // 2
-        System.out.println(5 / 2);
+        System.out.println(5 / 2); // 2.5 -> 2
+        System.out.println(2 / 4); // 0.5 -> 0
+        System.out.println(4 % 2); // 0 나머지를 계산
+        System.out.println(5 % 2); // 1
+
+        // 우선 순위 연산
+        System.out.println(2 + 2 * 2); // 6
+        System.out.println((2 + 2) * 2); // 8
+        System.out.println(2 + (2 * 2)); // 6 괄호를 이용하여 표현 가능
+
+        // 변수를 이용한 연산
+        int a = 20;
+        int b = 10;
+        int c;
+
+        c = a + b;
+        System.out.println(c); // 30
+
+        c = a - b;
+        System.out.println(c); // 10
+
+        c = a * b;
+        System.out.println(c); // 200
+
+        c = a / b;
+        System.out.println(c); // 2
+
+        c = a % b;
+        System.out.println(c); // 0
+
+        // 증감 연산 ++, --
+        int val;
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(++val); // 11
+        System.out.println(val); // 11
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val++); // 10
+        System.out.println(val); // 11
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(--val); // 9
+        System.out.println(val); // 9
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val--); // 10
+        System.out.println(val); // 9
+
+        // 은행 대기번호표
+        int waiting = 0;
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 0
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 1
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 2
+        System.out.println("총 대기 인원 : " + waiting); // 대기 인원 : 3
     }
 }
