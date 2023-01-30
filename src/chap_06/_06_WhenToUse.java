116b1613717a97d75b39a0138c416799bec57fa2
koojiny <msj09252@gmail.com>
2023-01-30 20:02:07 +0900

    Day 9 : Method

---
+++ src/chap_06/_06_WhenToUse.java
@@ -0,0 +1,41 @@
+package chap_06;
+
+public class _06_WhenToUse {
+    public static int getPower(int number) {
+//        int result = number * number;
+//        return result;
+        // return number * number; // 위의 두 줄과 같은 의미
+        return getPower(number, 2);
+    }
+
+    public static int getPower(int number, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= number;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // 메소드가 필요한 이유
+
+        // 2의 2승을 구하기
+        System.out.println(getPower(2, 2)); // 2 * 2 = 4
+
+        // 3의 3승을 구하기
+//        result = 1;
+//        for (int i = 0; i < 3; i++) {
+//            result *= 3;
+//        }
+//        System.out.println(result); // 3 * 3 * 3 = 27
+        System.out.println(getPower(3, 3));
+
+        // 4의 2승을 구하기
+//        result = 1;
+//        for (int i = 0; i < 2; i++) {
+//            result *= 4;
+//        }
+//        System.out.println(result); // 4 * 4 = 16
+        System.out.println(getPower(4,  2));
+    }
+}
