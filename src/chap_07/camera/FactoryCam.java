200d6803bc5da9864e15c02625662b9c3c151579
koojiny <msj09252@gmail.com>
2023-02-03 18:43:33 +0900

    Day 13 : Class (Super)

--- src/chap_07/camera/FactoryCam.java
+++ src/chap_07/camera/FactoryCam.java
@@ -4,7 +4,14 @@ package chap_07.camera;
 public class FactoryCam extends Camera { // 자식 클래스
 
     public FactoryCam() {
-        this.name = "공장 카메라";
+        // this.name = "공장 카메라";
+        super("공장 카메라"); // 부모 클래스의 생성자에 바로 접근
+    }
+
+    public void recordVideo() {
+        // 동영상 녹화
+        super.recordVideo(); // 부모 클래스에 있는 recordVideo 메소드를 수행
+        detectFire();
     }
 
     public void detectFire() {
